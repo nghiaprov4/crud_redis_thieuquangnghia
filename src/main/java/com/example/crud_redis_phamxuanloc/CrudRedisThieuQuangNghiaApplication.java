d495d380652e1299b88f7326c1b3deb1cfda3bf2
IBM <thieuquangnghia2000@gmail.com>
2021-11-09 21:15:02 +0700

    thieuquangnghia

---
+++ src/main/java/com/example/crud_redis_phamxuanloc/CrudRedisThieuQuangNghiaApplication.java
@@ -0,0 +1,26 @@
+package com.example.crud_redis_phamxuanloc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootApplication
+public class CrudRedisThieuQuangNghiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudRedisThieuQuangNghiaApplication.class, args);
+    }
+    @Bean
+    LettuceConnectionFactory jedisConnectionFactory(){
+
+        return new LettuceConnectionFactory();
+    }
+    @Bean
+    RedisTemplate redisTemplate(){
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        return redisTemplate;
+    }
+}
---
+++ src/test/java/com/example/crud_redis_phamxuanloc/CrudRedisThieuQuangNghiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_redis_phamxuanloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRedisThieuQuangNghiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
